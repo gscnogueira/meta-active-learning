5916b4f380e87d3793731ae3c84beedf1a506615
Gabriel da Silva Corvino Nogueira <gabriel.nogueira@tc.df.gov.br>
2024-04-04 18:16:46 -0300

    ✨ Adiciona comparação de metamodelo com demais modelos para um dataset

--- notebooks/experiments/active_learning.py
+++ notebooks/experiments/active_learning.py
@@ -2,6 +2,7 @@ import pickle as pkl
 import warnings
 from functools import partial
 
+from tqdm import tqdm
 from modAL.models import ActiveLearner, Committee
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
@@ -11,7 +12,20 @@ import numpy as np
 import pandas as pd
 import openml
 
+from modAL import uncertainty as u, batch as b
+from modAL import disagreement as d
+
 class ActiveLearningExperiment:
+
+    DISAGREEMENT_STRATEGIES = [d.max_disagreement_sampling,
+                               d.vote_entropy_sampling,
+                               d.consensus_entropy_sampling] 
+
+    UNCERTAINTY_STRATEGIES = [u.uncertainty_sampling,
+                              u.margin_sampling,
+                              u.entropy_sampling,
+                              b.uncertainty_batch_sampling]
+    
     def __init__(self, dataset_id, l_size, random_state=None):
 
         X, y = self.__load_data(dataset_id)
@@ -58,7 +72,6 @@ class ActiveLearningExperiment:
                                 query_strategy=args['query_strategy'])
 
         scores = []
-        
         for idx in range(n_queries):
 
             u_pool_size = np.size(u_y_pool)
@@ -80,6 +93,106 @@ class ActiveLearningExperiment:
 
         return scores
 
+    def run_topline(self, estimator, n_queries,
+                    query_strategies: list,
+                    batch_size=5, committee_size=3):
+
+        l_X_pool = self.X_train[self.labeled_index]
+        l_y_pool = self.y_train[self.labeled_index]
+
+        u_X_pool = np.delete(self.X_train, self.labeled_index, axis=0)
+        u_y_pool = np.delete(self.y_train, self.labeled_index, axis=0)
+
+        scores = []
+
+        for idx in tqdm(range(n_queries)):
+
+            u_pool_size = np.size(u_y_pool)
+
+            if u_pool_size <= 0:
+                break
+
+            query_index, score = self.__topline_query(
+                estimator=estimator,
+                query_strategies=query_strategies,
+                l_pool=(l_X_pool, l_y_pool),
+                u_pool=(u_X_pool, u_y_pool),
+                batch_size=5,
+                committee_size=committee_size)
+
+            new_X, new_y = u_X_pool[query_index], u_y_pool[query_index]
+
+            l_X_pool = np.append(l_X_pool, new_X, axis=0)
+            l_y_pool = np.append(l_y_pool, new_y, axis=0)
+
+            u_X_pool = np.delete(u_X_pool, query_index, axis=0)
+            u_y_pool = np.delete(u_y_pool, query_index, axis=0)
+
+            scores.append(score)
+
+        return scores
+
+    def __topline_query(self, estimator,
+                        l_pool, u_pool,
+                        query_strategies,
+                        batch_size, committee_size):
+
+        args = dict()
+        args['estimator'] = estimator
+        args['X_training'], args['y_training'] = l_pool
+
+        active_learners = [self.__gen_learner(query_strategy=s,
+                                              batch_size=batch_size,
+                                              committee_size=committee_size,
+                                              **args)
+                           for s in query_strategies]
+
+        best_score = 0
+        best_sample = None
+        u_X_pool, u_y_pool = u_pool
+
+        u_pool_size = np.size(u_y_pool)
+
+        for i, learner in enumerate(active_learners):
+
+            query_index = (learner.query(u_X_pool)[0]
+                           if u_pool_size > batch_size + 1
+                           else np.arange(u_pool_size))
+
+            learner.teach(X=u_X_pool[query_index], y=u_y_pool[query_index])
+
+            score = learner.score(self.X_test, self.y_test)
+
+            if score > best_score:
+                best_score = score
+                best_sample = query_index
+
+        return best_sample, best_score
+
+    def __gen_learner(self, query_strategy, committee_size, batch_size, **kwargs):
+
+        if query_strategy in self.UNCERTAINTY_STRATEGIES:
+            query_strategy = partial(query_strategy, n_instances=batch_size)
+
+            learner = ActiveLearner(query_strategy=query_strategy, **kwargs)
+
+            return learner
+
+        elif query_strategy in self.DISAGREEMENT_STRATEGIES:
+
+            query_strategy = partial(query_strategy, n_instances=batch_size)
+
+            learner_list = [ActiveLearner(**kwargs)
+                            for _ in range(committee_size)]
+
+            learner = Committee(learner_list=learner_list,
+                                query_strategy=query_strategy)
+            return learner
+
+        else:
+            raise ValueError("Estratégia de sampling não suportada")
+
+
     def __load_data(self, dataset_id):
 
         with warnings.catch_warnings():
@@ -99,55 +212,27 @@ class ActiveLearningExperiment:
         return X, y
 
 
-from modAL.uncertainty import uncertainty_sampling, margin_sampling, entropy_sampling
-from modAL.disagreement import (consensus_entropy_sampling,
-                                max_disagreement_sampling,
-                                vote_entropy_sampling)
-from modAL.batch import uncertainty_batch_sampling
-
-def meta_sampling(classifier, X_pool, n_instances):
-    query_strategies = {
-        'consensus_entropy_sampling': consensus_entropy_sampling,
-        'entropy_sampling': entropy_sampling,
-        'margin_sampling': margin_sampling,
-        'max_disagreement_sampling': max_disagreement_sampling,
-        'uncertainty_batch_sampling': uncertainty_batch_sampling,
-        'uncertainty_sampling': uncertainty_sampling,
-        'vote_entropy_sampling': vote_entropy_sampling
-    }
-
-    # carregando modelo
-    with open('meta_model.pkl', 'rb') as f:
-        model = pkl.load(f)
-
-    # extração de metafeatures
-    with warnings.catch_warnings():
-        warnings.filterwarnings('ignore')
-        mfe = MFE(groups='all')
-        mfe.fit(X_pool)
-        names, mfts = mfe.extract()
-
-    X = [mfts]
-
-    selected_strategy = model.predict(X)[0]
-
-    print(selected_strategy)
-
-    return query_strategies[selected_strategy](classifier, X_pool,
-                                               n_instances=n_instances)
-
 if __name__ == "__main__":
 
     from sklearn.svm import SVC
-    from modAL.uncertainty import uncertainty_sampling
-    from modAL.disagreement import max_disagreement_sampling
+    from modAL import uncertainty as u
+    from modAL import disagreement as d
+    from modAL import batch as b
 
     exp = ActiveLearningExperiment(dataset_id=991,
                                    random_state=42,
                                    l_size=5)
 
-    scores = exp.run(estimator=SVC(probability=True),
-            committee_size=3,
-            query_strategy=uncertainty_sampling)
-    
-    print(scores)
+    query_strategies = [u.uncertainty_sampling,
+                        u.margin_sampling,
+                        u.entropy_sampling,
+                        b.uncertainty_batch_sampling,
+                        d.max_disagreement_sampling,
+                        d.consensus_entropy_sampling,
+                        d.vote_entropy_sampling]
+
+    topline_scores = exp.run_topline(estimator=SVC(probability=True),
+                                     query_strategies=query_strategies,
+                                     n_queries=100,
+                                     batch_size=5)
+    print(topline_scores)
