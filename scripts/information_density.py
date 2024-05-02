feb181b1d6d0dd94bd7328391b7e1979d81b25b2
Gabriel da Silva Corvino Nogueira <gabriel.nogueira@tc.df.gov.br>
2024-05-02 17:01:15 -0300

    ✨ Adiciona a estratégia TU

--- scripts/information_density.py
+++ scripts/information_density.py
@@ -1,3 +1,4 @@
+from sklearn.metrics.pairwise import pairwise_distances
 from modAL.density import information_density
 from modAL.uncertainty import classifier_margin
 from modAL.utils.selection import multi_argmax
@@ -15,3 +16,20 @@ def density_weighted_sampling(learner, X, n_instances=1):
     dw_margin = density_weighted(learner, X)
 
     return multi_argmax(dw_margin, n_instances=n_instances)
+
+
+def training_utility(learner, X_unlabeled, X_labeled):
+
+    dw = density_weighted(learner, X_unlabeled)
+
+    similarity_mtx = 1/(1 + pairwise_distances(X_unlabeled, X_labeled))
+    tu = similarity_mtx.mean(axis=1)
+
+    return dw/tu
+
+
+def training_utility_sampling(learner, X_unlabeled, X_labeled, n_instances=1):
+
+    tu = training_utility(learner, X_unlabeled, X_labeled)
+
+    return multi_argmax(tu, n_instances=n_instances)
