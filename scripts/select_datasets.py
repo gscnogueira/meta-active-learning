3e95187ecba4dafa687595e350be01800fb666ee
Gabriel da Silva Corvino Nogueira <gabriel.nogueira@tc.df.gov.br>
2024-05-03 17:17:44 -0300

    ♻️ Refatora script de seleção de bases

--- scripts/select_datasets.py
+++ scripts/select_datasets.py
@@ -12,27 +12,9 @@ from sklearn.model_selection import cross_validate, StratifiedKFold
 
 
 
-already_created = [
-    991,
-    917,
-    799,
-    740,
-    44620,
-    44616,
-    44493,
-    44433,
-    44422,
-    44397,
-    44383,
-    44382,
-    44370,
-    40704,
-    1016,
-]
-
 
 def get_dataset_ids(max_number_of_instances=10000,
-                    min_number_of_instances=100,
+                    min_number_of_instances=300,
                     max_number_of_features=500,
                     max_number_of_instances_with_missing_values=0,
                     max_number_of_classes=5,
@@ -55,6 +37,7 @@ def get_dataset_ids(max_number_of_instances=10000,
 
 def test_dataset(dataset_id):
 
+    print('Iniciando para', dataset_id)
     with warnings.catch_warnings():
         warnings.simplefilter('ignore')
         dataset = openml.datasets.get_dataset(dataset_id)
@@ -66,40 +49,37 @@ def test_dataset(dataset_id):
                 target=dataset.default_target_attribute)
 
             categorical_encoder = OneHotEncoder(handle_unknown='infrequent_if_exist')
-            
+
             transformer = ColumnTransformer([
                 ('onehot_encoder', categorical_encoder, categorical_indicator)],
                                             remainder='passthrough'
                                             )
-            
+
             model = make_pipeline(transformer, RandomForestClassifier())
-            
+
             skf = StratifiedKFold(n_splits=5)
 
             cross_validate(model, X, y,
                            cv=skf,
                            error_score='raise')
-    except Exception as e:
-        return False
+    except Exception:
+        return True
 
-    return True
+    print('Fim para', dataset_id)
+    return False
 
 if __name__ == '__main__':
 
-    N_DATASETS = 100 - len(already_created)
-
+    N_DATASETS = 100
 
     dataset_ids = get_dataset_ids()
 
     with Pool() as pool:
         results = pool.map(test_dataset, dataset_ids)
 
-    filtered_ids = [id for id, result in zip(dataset_ids, results)
-                    if result and id not in already_created]
-
     random.seed(42)
 
-    selected_ids = sorted(random.sample(filtered_ids, N_DATASETS))
+    selected_ids = sorted(random.sample(dataset_ids, N_DATASETS))
 
     with open('selected_dataset_ids.txt', 'w') as arquivo:
         for id in selected_ids:
