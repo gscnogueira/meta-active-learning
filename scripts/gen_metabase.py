fe83b4383887ffccff27d704e5641381db5aa53d
Gabriel da Silva Corvino Nogueira <gabriel.nogueira@tc.df.gov.br>
2024-05-21 14:05:43 -0300

    🧵 (Geração de Metabase) suporte a paralelismo para nova configuração

--- scripts/gen_metabase.py
+++ scripts/gen_metabase.py
@@ -1,12 +1,14 @@
-from os import environ
-# environ['OMP_NUM_THREADS'] = '1'
+"""Script para a geração de metabase."""
 
-import time
-import sys
 from functools import partial
-from multiprocessing import Pool, get_context
+from multiprocessing import get_context
+from os import environ
 import logging
 import os
+import re
+import sys
+
+environ['OMP_NUM_THREADS'] = '1'
 
 from tqdm import tqdm
 from sklearn.svm import SVC
@@ -16,9 +18,7 @@ from sklearn.naive_bayes import GaussianNB
 from modAL.uncertainty import margin_sampling
 
 from meta_base_builder import MetaBaseBuilder
-from expected_error import expected_error_reduction
-from information_density import (density_weighted_sampling,
-                                 training_utility_sampling)
+import config
 
 DOWNLOAD_PATH = '../metabase/'
 
@@ -29,10 +29,8 @@ estimator_dict = {
     "SVC": SVC,
 }
 
-ERROR_LIST = []
 
-def gen_metabase(dataset_id,
-                 estimator_name, 
+def gen_metabase(dataset_id, estimator_name,
                  query_strategies,
                  random_state,
                  initial_labeled_size,
@@ -43,7 +41,7 @@ def gen_metabase(dataset_id,
 
     pid = os.getpid()
 
-    context_string = f'{dataset_id}, {estimator}, {pid}'
+    context_string = f'{os.path.basename(dataset_id)}, {estimator}, {pid}'
 
     try:
         logging.warning(f'[{context_string}] Iniciando construção de metabase.')
@@ -68,52 +66,63 @@ def gen_metabase(dataset_id,
         logging.warning(f'[{context_string}] Metabase construida.')
 
     except Exception as e:
-        ERROR_LIST.append(dataset_id)
         logging.error(f'[{context_string}] Ocorreu um erro: {e}')
 
+
+def get_datasets(datasets_path, experiments_path):
+    """
+    Retorna conjuntos de dados a serem utilizasos no experimento.
+
+    Os arquivos ARFFs contidos no diretório datasets representam um
+    número maior de conjuntos do que os utilizados nos experimentos do
+    Davi. Dessa forma, essa função visa se utilizar dos dados
+    experimentais contidos na pasta CSV, para que seja possivel
+    filtrá-los
+    """
+
+    csv_name_pattern = r"(.*)-r\d-f\d-normalized-pool.arff.csv$"
+    file_names = [os.path.basename(f) for f in os.listdir(datasets_path)]
+
+    csv_names = {re.search(csv_name_pattern, file_name).group(1)
+                 for file_name in os.listdir(experiments_path)}
+
+    dataset_ids = [f_name for f_name in file_names
+                   if os.path.splitext(f_name)[0] in csv_names]
+
+    return dataset_ids
 
 
 if __name__ == '__main__':
 
+    N_WORKERS = 8  # MUDAR DEPOIS
     DATASETS_PATH = '../datasets'
+    EXPERIMENTS_PATH = '../resultados_davi'
+
+    kwargs = {
+        'query_strategies': config.query_strategies,
+        'initial_labeled_size': 5,
+        'n_queries': 100,
+        'batch_size': 1,
+        'random_state': 42
+    }
+
     logging.basicConfig(level=logging.WARNING,
                         format='%(asctime)s:%(levelname)s:%(message)s')
 
-    clf_list = ["KNN", "GaussianNB"]
+    dataset_ids = [os.path.join(DATASETS_PATH, f_name) for f_name in
+                   get_datasets(DATASETS_PATH, EXPERIMENTS_PATH)]
 
-    query_strategies = [
-        training_utility_sampling,
-        density_weighted_sampling,
-        margin_sampling,
-        expected_error_reduction]
+    print(f'Foram encontrados {len(dataset_ids)} datasets.')
 
-    gen_metabase_partial = partial(
-        gen_metabase,
-        query_strategies=query_strategies,
-        initial_labeled_size=5,
-        n_queries=2,  # NAO ESQUECE DE MUDAR PRA 100 DE NOVO!!!!!!
-        batch_size=1,
-        random_state=42)
+    for estimator_name in config.classifier_list:
 
-    dataset_ids = list(os.path.join(DATASETS_PATH, f)
-                       for f in os.listdir(DATASETS_PATH))
-
-    n_workers = 1  # MUDAR DEPOIS
-
-    for estimator_name in clf_list:
-        t = time.time()
         print(f'Iniciando Criação de metabase para {estimator_name}')
-        for d in tqdm(dataset_ids):
-            gen_metabase_partial(d, estimator_name)
-        t = time.time() - t
-        print(f'{estimator_name}: Done in {t} seconds!')
-        break
 
-    print(ERROR_LIST)
+        gen_metabase_partial = partial(
+            gen_metabase, estimator_name=estimator_name, **kwargs)
 
-    exit()
-
-    with get_context("fork").Pool(n_workers) as p:
-        results = [e for e in tqdm(p.imap_unordered(gen_metabase_partial, args),
-                                   total=len(args),
-                                   file=sys.stdout)]
+        with get_context("fork").Pool(N_WORKERS) as p:
+            generator = p.imap_unordered(gen_metabase_partial, dataset_ids)
+            pbar = tqdm(generator, total=len(dataset_ids),
+                        file=sys.stdout, desc=estimator_name)
+            any(pbar)
